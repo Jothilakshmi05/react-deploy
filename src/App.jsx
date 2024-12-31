36014086f00a123676bbfecb9c49093a03918bce
Jothi <jothiramar2005@gmail.com>
2024-12-31 10:41:50 +0530

    route name changed

--- src/App.jsx
+++ src/App.jsx
@@ -25,7 +25,7 @@ const App = () => {
       <div>
         <Navbar />
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path="/react-deploy" element={<Home />} />
           <Route path="/about" element={<About />} />
           <Route path="/services" element={<Services />} />
           <Route path="/contact" element={<Contact />} />
